e40d1f58e2ea3d76afab0c64e0c0b7fe52dc813c
sena <3737v@naver.com>
2025-05-15 12:36:25 +0900

    version4

--- chapter02/Branch01.java
+++ chapter02/Branch01.java
@@ -3,7 +3,8 @@ package chapter02;
 public class Branch01 {
     
     public Branch01(){
-        System.out.println("main에서 작업");
+
+        System.out.println("충돌 테스트");
         System.out.println("daum에서 작업");
     }
 }
